0da1507266f25bdac6dc485985ac056cd3025451
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-11-04 03:19:55 +0330

    Add AdminDTO and ProfessorDTO and StudentDTO

---
+++ src/main/java/ir/maktab56/service/dto/ProfessorDTO.java
@@ -0,0 +1,27 @@
+package ir.maktab56.service.dto;
+
+import ir.maktab56.model.enumeration.UserType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfessorDTO {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private UserType userType;
+
+    private String nationalCode;
+
+    private Long personnelId;
+
+}
