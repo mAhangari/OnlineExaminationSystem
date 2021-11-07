94e7cde0cb549dea320b87e32b799fd02e9d3606
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-11-07 21:11:40 +0330

    Update AdminRepository make it bean

--- src/main/java/ir/maktab56/repository/AdminRepository.java
+++ src/main/java/ir/maktab56/repository/AdminRepository.java
@@ -1,6 +1,8 @@
 package ir.maktab56.repository;
 
 import ir.maktab56.model.Admin;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepository extends UserRepository<Admin> {
 }
