03b2cf5de43ea60d157a808307d3cdbecb4f67b8
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-11-07 21:18:58 +0330

    Update AdminService and AdminServiceImpl add initAdmin

--- src/main/java/ir/maktab56/service/AdminService.java
+++ src/main/java/ir/maktab56/service/AdminService.java
@@ -3,4 +3,6 @@ package ir.maktab56.service;
 import ir.maktab56.model.Admin;
 
 public interface AdminService extends UserService<Admin> {
+
+    void initAdmin();
 }
--- src/main/java/ir/maktab56/service/impl/AdminServiceImpl.java
+++ src/main/java/ir/maktab56/service/impl/AdminServiceImpl.java
@@ -1,14 +1,38 @@
 package ir.maktab56.service.impl;
 
 import ir.maktab56.model.Admin;
-import ir.maktab56.repository.UserRepository;
+import ir.maktab56.model.enumeration.UserType;
+import ir.maktab56.repository.AdminRepository;
 import ir.maktab56.service.AdminService;
+import ir.maktab56.service.RoleService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminServiceImpl extends UserServiceImpl<Admin> implements AdminService {
 
-    public AdminServiceImpl(UserRepository<Admin> repository) {
+    private final AdminRepository repository;
+    private final RoleService roleService;
+
+    public AdminServiceImpl(AdminRepository repository, RoleService roleService) {
         super(repository);
+        this.repository = repository;
+        this.roleService = roleService;
+    }
+
+    @Override
+    public void initAdmin() {
+        if (repository.count() == 0) {
+            Admin admin = new Admin();
+            admin.setFirstName("Morteza");
+            admin.setLastName("Ahangari");
+            admin.setUsername("mAhangari");
+            admin.setPassword(new BCryptPasswordEncoder().encode("Dan9011216"));
+            admin.setNationalCode("2150382342");
+            admin.setUserType(UserType.ADMIN);
+            admin.setPersonnelId(9814163102L);
+            admin.setRoles(roleService.findRoleByName("ADMIN").orElse(null));
+            save(admin);
+        }
     }
 }
