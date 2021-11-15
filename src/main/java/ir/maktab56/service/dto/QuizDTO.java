b4349f33ccac6cb7a0e39603880cfd54d5856ec9
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-11-15 21:21:42 +0330

    Modify QuizDTO add validation fields

--- src/main/java/ir/maktab56/service/dto/QuizDTO.java
+++ src/main/java/ir/maktab56/service/dto/QuizDTO.java
@@ -5,6 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -13,9 +18,15 @@ public class QuizDTO {
 
     private Long id;
 
+    @NotNull
+    @NotBlank(message = "Title is mandatory")
     private String title;
 
+    @NotNull
+    @NotBlank(message = "Description is mandatory")
     private String description;
 
+    @Min(value = 0, message = "Minimum value must be zero")
+    @Max(value = 360, message = "Maximum value must be 360")
     private Long quizTime;
 }
