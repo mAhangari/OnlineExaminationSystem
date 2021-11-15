dbe8373735a69be6f0467d6b65c71d53bbb0f1a8
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-11-16 03:10:35 +0330

    Add QuestionDTO

---
+++ src/main/java/ir/maktab56/service/dto/QuestionDTO.java
@@ -0,0 +1,26 @@
+package ir.maktab56.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class QuestionDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Title is mandatory")
+    private String title;
+
+    @NotBlank(message = "Question is mandatory")
+    private String question;
+
+    private double score;
+
+}
