27193b7b29605ac6e766011431b69e9209af4535
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-11-16 03:41:20 +0330

    Modify Question add validation

--- src/main/java/ir/maktab56/model/Question.java
+++ src/main/java/ir/maktab56/model/Question.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = Question.TABLE_NAME)
@@ -19,9 +20,15 @@ public class Question extends BaseEntity<Long> {
     public static final String TABLE_NAME = "question_table";
     public static final String QUESTION_BANK_ID = "question_bank_id";
     public static final String PROFESSOR_ID = "professor_id";
+    public static final String TITLE = "title";
 
+    @Column(name = TITLE, nullable = false)
+    @NotBlank
     private String title;
+
+    @Column(nullable = false)
     private String question;
+
     private double score;
 
     @ManyToOne
