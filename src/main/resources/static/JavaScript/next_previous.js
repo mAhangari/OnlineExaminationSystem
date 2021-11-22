c9c8ff39d491b4f943fbc2b0b149d7d7bdea606a
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-11-22 06:54:44 +0330

    Modify next_previous.js correct typos

--- src/main/resources/static/JavaScript/next_previous.js
+++ src/main/resources/static/JavaScript/next_previous.js
@@ -20,54 +20,54 @@ $(document).ready(function () {
             } else if (circle_id === 0 && (res_length / $rowPerPage) <= 3 && (res_length / $rowPerPage) > 2) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
                 $(`#circle_id_${circle_id + 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id + 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id + 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("little_circle");
                 circle_id++;
             } else if (circle_id === 0 && (res_length / $rowPerPage) > 3 && (res_length / $rowPerPage) <= 4) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
                 $(`#circle_id_${circle_id + 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id + 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id + 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("little_circle");
                 $(`#circle_id_${circle_id + 3}`).removeClass("display_none");
                 circle_id++;
             } else if (circle_id === 0 && (res_length / $rowPerPage) > 4) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
                 $(`#circle_id_${circle_id + 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id + 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id + 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("little_circle");
                 $(`#circle_id_${circle_id + 3}`).removeClass("display_none");
                 circle_id++;
             } else if (circle_id === 1 && (res_length / $rowPerPage) <= 3 && (res_length / $rowPerPage) > 2) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id - 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id - 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id + 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
                 circle_id++;
             } else if (circle_id === 1 && (res_length / $rowPerPage) > 3 && (res_length / $rowPerPage) <= 4) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id - 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id - 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id + 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id + 2}`).addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id + 2}`).addClass("middle_circle").removeClass("little_circle");
                 circle_id++;
             } else if (circle_id === 1 && (res_length / $rowPerPage) > 4) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id - 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id - 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id + 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id + 2}`).addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id + 2}`).addClass("middle_circle").removeClass("little_circle");
                 $(`#circle_id_${circle_id + 3}`).removeClass("display_none");
                 circle_id++;
             } else if (circle_id === 2 && (res_length / $rowPerPage) > 3 && (res_length / $rowPerPage) <= 4) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id - 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id - 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id + 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
                 $(`#circle_id_${circle_id - 2}`).addClass("display_none");
                 circle_id++;
             } else if (circle_id === 2 && (res_length / $rowPerPage) > 4 && ($end + $rowPerPage) >= res_length) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id - 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id - 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id - 2}`).addClass("display_none");
                 $(`#circle_id_${circle_id + 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id + 2}`).addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id + 2}`).addClass("middle_circle").removeClass("little_circle");
                 circle_id++;
             } else if (circle_id === 3 && (res_length / $rowPerPage) > 4 && $end >= res_length) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id - 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id - 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id - 2}`).addClass("display_none");
                 $(`#circle_id_${circle_id + 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
                 circle_id++;
@@ -91,54 +91,54 @@ $(document).ready(function () {
             } else if (circle_id === 2 && (res_length / $rowPerPage) <= 3 && (res_length / $rowPerPage) > 2) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
                 $(`#circle_id_${circle_id - 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id - 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id - 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("little_circle");
                 circle_id--;
             } else if (circle_id === 3 && (res_length / $rowPerPage) > 3 && (res_length / $rowPerPage) <= 4) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
                 $(`#circle_id_${circle_id - 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id - 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id - 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("little_circle");
                 $(`#circle_id_${circle_id - 3}`).removeClass("display_none");
                 circle_id--;
             } else if (circle_id === 4 && (res_length / $rowPerPage) > 4) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
                 $(`#circle_id_${circle_id - 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id - 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id - 2}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("little_circle");
                 $(`#circle_id_${circle_id - 3}`).removeClass("display_none");
                 circle_id--;
             } else if (circle_id === 1 && (res_length / $rowPerPage) <= 3 && (res_length / $rowPerPage) > 2) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id + 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id + 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id - 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
                 circle_id--;
             } else if (circle_id === 2 && (res_length / $rowPerPage) > 3 && (res_length / $rowPerPage) <= 4) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id + 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id + 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id - 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id - 2}`).addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id - 2}`).addClass("middle_circle").removeClass("little_circle");
                 circle_id--;
             } else if (circle_id === 3 && (res_length / $rowPerPage) > 4) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id + 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id + 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id - 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id - 2}`).addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id - 2}`).addClass("middle_circle").removeClass("little_circle");
                 $(`#circle_id_${circle_id - 3}`).removeClass("display_none");
                 circle_id--;
             } else if (circle_id === 1 && (res_length / $rowPerPage) > 3 && (res_length / $rowPerPage) <= 4) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id + 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id + 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id - 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
                 $(`#circle_id_${circle_id + 2}`).addClass("display_none");
                 circle_id--;
             } else if (circle_id === 2 && (res_length / $rowPerPage) > 4 && ($start - $rowPerPage) < $rowPerPage) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id + 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id + 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id + 2}`).addClass("display_none");
                 $(`#circle_id_${circle_id - 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
-                $(`#circle_id_${circle_id - 2}`).addClass("middle_circle").removeClass("litle_circle");
+                $(`#circle_id_${circle_id - 2}`).addClass("middle_circle").removeClass("little_circle");
                 circle_id--;
             } else if (circle_id === 1 && (res_length / $rowPerPage) > 4 && $start < $rowPerPage) {
                 $(`#circle_id_${circle_id}`).css("backgroundColor", "white").addClass("middle_circle").removeClass("circle");
-                $(`#circle_id_${circle_id + 1}`).addClass("litle_circle").removeClass("middle_circle");
+                $(`#circle_id_${circle_id + 1}`).addClass("little_circle").removeClass("middle_circle");
                 $(`#circle_id_${circle_id + 2}`).addClass("display_none");
                 $(`#circle_id_${circle_id - 1}`).addClass("circle").removeClass("middle_circle").css("backgroundColor", "gray");
                 circle_id--;
@@ -163,14 +163,14 @@ $(document).ready(function () {
             } else if ((res_length / $rowPerPage) > 2) {
                 tr.push($(`<span id="circle_id_${i++}" class='circle'>`)[0]);
                 tr.push($(`<span id="circle_id_${i++}" class='middle_circle'>`)[0]);
-                tr.push($(`<span id="circle_id_${i++}" class='litle_circle'>`)[0]);
+                tr.push($(`<span id="circle_id_${i++}" class='little_circle'>`)[0]);
             }
 
             if ((res_length / $rowPerPage) > 3 && (res_length / $rowPerPage) <= 4) {
-                tr.push($(`<span id="circle_id_${i++}" class='litle_circle display_none'>`)[0]);
+                tr.push($(`<span id="circle_id_${i++}" class='little_circle display_none'>`)[0]);
             } else if ((res_length / $rowPerPage) > 4) {
-                tr.push($(`<span id="circle_id_${i++}" class='litle_circle display_none'>`)[0]);
-                tr.push($(`<span id="circle_id_${i++}" class='litle_circle display_none'>`)[0]);
+                tr.push($(`<span id="circle_id_${i++}" class='little_circle display_none'>`)[0]);
+                tr.push($(`<span id="circle_id_${i++}" class='little_circle display_none'>`)[0]);
             }
 
             tr.push($(`<button id="next" class="previousAndNextButton" type="button" onclick='$(this).next_func()'>NEXT</button>`)[0]);
