249f96e0739c24bae170445cb253ca07e8f6c054
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-11-23 16:02:23 +0330

    Modify OutOfTimeException change error code type to ErrorCode

--- src/main/java/ir/maktab56/exception/OutOfTimeException.java
+++ src/main/java/ir/maktab56/exception/OutOfTimeException.java
@@ -1,5 +1,7 @@
 package ir.maktab56.exception;
 
+import ir.maktab56.exception.enumeration.ErrorCode;
+
 import java.io.Serial;
 
 public class OutOfTimeException extends RuntimeException {
@@ -7,29 +9,29 @@ public class OutOfTimeException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 7718828512143226455L;
 
-    private final String code;
+    private final ErrorCode code;
 
-    public OutOfTimeException(String code) {
+    public OutOfTimeException(ErrorCode code) {
         super();
         this.code = code;
     }
 
-    public OutOfTimeException(String message, Throwable cause, String code) {
+    public OutOfTimeException(String message, Throwable cause, ErrorCode code) {
         super(message, cause);
         this.code = code;
     }
 
-    public OutOfTimeException(String message, String code) {
+    public OutOfTimeException(String message, ErrorCode code) {
         super(message);
         this.code = code;
     }
 
-    public OutOfTimeException(Throwable cause, String code) {
+    public OutOfTimeException(Throwable cause, ErrorCode code) {
         super(cause);
         this.code = code;
     }
 
-    public String getCode() {
+    public ErrorCode getCode() {
         return this.code;
     }
 }
