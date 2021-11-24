00988d3070e4b5170b46da571961fb444c351a5a
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-11-24 07:57:38 +0330

    Update AnswerService and AnswerServiceImpl add find by student username... and set answer method

--- src/main/java/ir/maktab56/service/AnswerService.java
+++ src/main/java/ir/maktab56/service/AnswerService.java
@@ -1,7 +1,10 @@
 package ir.maktab56.service;
 
 import ir.maktab56.model.Answer;
+import ir.maktab56.service.dto.AnswerDTO;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface AnswerService {
@@ -10,4 +13,7 @@ public interface AnswerService {
 
     Optional<Answer> findByStudent_Username(String username);
 
+    Optional<Answer> findByStudent_UsernameAndQuestionSheet_Quiz_Id(String username, Long quizId);
+
+    AnswerDTO setAnswer(List<Map<String, Object>> answer);
 }
--- src/main/java/ir/maktab56/service/impl/AnswerServiceImpl.java
+++ src/main/java/ir/maktab56/service/impl/AnswerServiceImpl.java
@@ -1,13 +1,18 @@
 package ir.maktab56.service.impl;
 
+import ir.maktab56.mapper.AnswerMapper;
 import ir.maktab56.model.*;
 import ir.maktab56.repository.AnswerRepository;
 import ir.maktab56.service.AnswerService;
+import ir.maktab56.service.QuestionService;
+import ir.maktab56.service.dto.AnswerDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Service
@@ -15,6 +20,18 @@ import java.util.*;
 public class AnswerServiceImpl implements AnswerService {
 
     private final AnswerRepository repository;
+    private AnswerMapper answerMapper;
+    private QuestionService questionService;
+
+    @Autowired
+    public void setAnswerMapper(AnswerMapper answerMapper) {
+        this.answerMapper = answerMapper;
+    }
+
+    @Autowired
+    public void setQuestionService(QuestionService questionService) {
+        this.questionService = questionService;
+    }
 
     @Override
     public Answer save(Answer answer) {
@@ -25,6 +42,44 @@ public class AnswerServiceImpl implements AnswerService {
     public Optional<Answer> findByStudent_Username(String username) {
 
         return repository.findByStudent_Username(username);
+    }
+
+    @Override
+    public Optional<Answer> findByStudent_UsernameAndQuestionSheet_Quiz_Id(String username, Long quizId) {
+        return repository.findByStudent_UsernameAndQuestionSheet_Quiz_Id(username, quizId);
+    }
+
+    // set answer base on receive answer from student
+    @Override
+    public AnswerDTO setAnswer(List<Map<String, Object>> answer) {
+        Long quizId = Long.parseLong(answer.get(0).get("quizId").toString());
+        Map<Question, String> answers = createAnswers(answer);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Answer answer1 = findByStudent_UsernameAndQuestionSheet_Quiz_Id(authentication.getName(), quizId).orElseThrow();
+        answer1.setAnswer(answers);
+
+        if (answer1.getEndAnswerDateTime() == null) {
+            answer1.setEndAnswerDateTime(LocalDateTime.now());
+            save(answer1);
+        }
+        return answerMapper.convertEntityToDTO(answer1);
+    }
+
+    // map input answer to (Question, String) and then return
+    private Map<Question, String> createAnswers(List<Map<String, Object>> answer) {
+
+        Map<Question, String> answers = new HashMap<>();
+
+        for (int index = 1; index < answer.size(); index++) {
+            answers.put(
+                    questionService
+                            .findById(Long.parseLong(answer.get(index).get("questionId").toString())).orElseThrow(),
+                    answer.get(index).get("answer").toString()
+            );
+        }
+
+        return answers;
     }
 
 
